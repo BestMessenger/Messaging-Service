dfe6c60c392a86c3fb592373ce2c25019d87304d
lapkinvladimir <128398343+lapkinvladimir@users.noreply.github.com>
2024-03-02 03:30:07 +0100

    Add files via upload

--- src/main/java/com/messenger/message_service/service/MessageService.java
+++ src/main/java/com/messenger/message_service/service/MessageService.java
@@ -4,13 +4,18 @@ import com.messenger.message_service.dto.dtoMapper.DtoMapper;
 import com.messenger.message_service.dto.openfeignDto.GroupMembershipResponse;
 import com.messenger.message_service.dto.websocketDto.MessageRequest;
 import com.messenger.message_service.dto.websocketDto.MessageResponse;
+import com.messenger.message_service.encryption.MessageEncryptor;
 import com.messenger.message_service.exception.NoEntityFoundException;
 import com.messenger.message_service.models.MessageModel;
 import com.messenger.message_service.repository.MessageRepository;
 import com.messenger.message_service.utils.userChecker.UserChecker;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Base64;
 import java.util.List;
 
 @Service
@@ -49,15 +54,49 @@ public class MessageService {
         return dtoMapper.toResponseList(messageModels);
     }
 
+
+    @SneakyThrows
     public MessageResponse createMessage(MessageRequest messageRequest) {
-        MessageModel messageModel = dtoMapper.toModel(messageRequest);
-        GroupMembershipResponse membershipResponse = responseUserGroupChecker.isExistUserInProfileService(
-                messageRequest.getSenderId(), messageRequest.getGroupId());
-        if (membershipResponse != null) {
-            MessageModel savedMessage = messageRepository.save(messageModel);
-            return dtoMapper.toResponse(savedMessage);
+        MessageModel messageModel = MessageModel.builder()
+                .message(messageRequest.getMessage())
+                .groupId(messageRequest.getGroupId())
+                .senderId(messageRequest.getSenderId())
+                .sendDatetime(LocalDateTime.now())
+                .build();
+
+        byte[] key;
+        if (messageRequest.getGroupId() != null) {
+            key = MessageEncryptor.generateKey();
+        } else {
+            key = MessageEncryptor.generateKeyFromMessageAndSenderId(
+                    messageRequest.getMessage(), String.valueOf(messageRequest.getSenderId()));
         }
-        throw new NoEntityFoundException("the user " + messageModel.getSenderId() + " is not a member of group " + messageModel.getGroupId() + " or does not exist");
+
+        byte[] encryptedMessage = MessageEncryptor.encrypt(key, messageRequest.getMessage(), String.valueOf(messageRequest.getSenderId()));
+
+        String encryptedMessageBase64 = Base64.getEncoder().encodeToString(encryptedMessage);
+        messageModel.setMessage(encryptedMessageBase64);
+
+        MessageModel savedMessage = messageRepository.save(messageModel);
+
+        return dtoMapper.toResponse(savedMessage);
+    }
+
+    @SneakyThrows
+    public String getDecryptedMessage(Long messageId){
+        MessageModel messageModel = messageRepository.findById(messageId)
+                .orElseThrow(() -> new EntityNotFoundException("Message not found with ID: " + messageId));
+        byte[] key;
+        if (messageModel.getGroupId() != null) {
+            key = MessageEncryptor.generateKey();
+        } else {
+            key = MessageEncryptor.generateKeyFromMessageAndSenderId(
+                    messageModel.getMessage(), String.valueOf(messageModel.getSenderId()));
+        }
+
+        String decryptedMessage = MessageEncryptor.decrypt(key, messageModel.getMessage());
+
+        return decryptedMessage;
     }
 
 }
